9172a797dd3fd45b8eb3a269e0180a64dadbcb9a
Giovani Garfias <46204471+survani@users.noreply.github.com>
2020-03-12 03:16:25 -0700

    :sparkles: post working correctly. Looking to fix a lot of other requests. One Step closer to MVP.

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ let users = [
     bio: "Web Developer"
   },
   {
-    id: shortid.generate(),
+    id: "",
     name: "Mark Garfias",
     bio: "Not related to Giovani Garfias"
   }
@@ -24,21 +24,69 @@ server.listen(4000, () => {
 
 server.use(express.json());
 
-//GET for init db
+//GET
 server.get("/", (req, res) => {
   res.send("Welcome to the user api!");
 });
 
-server.get("/users", (req, res) => {
-  res.status(200).json(users);
+//FIXME: Work on better logic *WIP*
+server.get("/api/users", (req, res) => {
+  if (res.status() === res.status(200)) {
+    res.status(200).json(users);
+  } else {
+    res.status(500).json({
+      errorMessage: "There was an error while saving the user to the database"
+    });
+  }
 });
 
-//post request
-server.post("/users", (req, res) => {
-  const userInfo = req.body;
-  userInfo.id = shortid.generate();
-  users.push(userInfo);
+//TODO: => get with :id
+server.get("/api/users/:id", (req, res) => {
+  res.send("Specific User");
+});
 
-  //returns a status
-  res.status(201).json(userInfo);
+//post request - path - req, res
+server.post("/api/users", (req, res) => {
+  //if and else statement
+  // if the request has a name && bio then post a "freshUser"
+  if (req.body.name && req.body.bio) {
+    const freshUser = {
+      id: shortid.generate(),
+      name: req.body.name,
+      bio: req.body.bio
+    };
+
+    //now push freshUsers information gathered into users: [].
+    users.push(freshUser);
+
+    //if successful res with a 201 code. Plus add what was added [freshUser]
+    res.status(201).json(freshUser);
+
+    //if this fails because name or bio are missing return a 400.
+  } else {
+    if (!req.body.name || !req.body.bio) {
+      res
+        .status(400)
+        .json({ errorMessage: "Please provide name and bio for the user." });
+
+      //if everything goes wrong then produce a 500 code error!
+    } else {
+      res.status(500).json({ errorNotification: "Couldn't save the user..." });
+    }
+  }
+});
+
+//TODO: post with :id
+server.post("/api/users/:id", (req, res) => {
+  res.send("Add User with an id");
+});
+
+//TODO: => delete with :id
+server.delete("/api/users/:id", (req, res) => {
+  res.send("User Deleted");
+});
+
+//TODO: => updated with :id
+server.put("/api/users/:id", (req, res) => {
+  res.send("User Updated");
 });
